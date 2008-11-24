340a4728cfed99ebb8fcb54374e1363b588d0ddb
mysqldev <mysqldev@gui-team-win>
2008-11-24 19:46:05 +0100

    Remove inclusion of unneeded header

--- driver/mysql_constructed_resultset.cpp
+++ driver/mysql_constructed_resultset.cpp
@@ -19,7 +19,6 @@
    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-#include "mysql_res_wrapper.h"
 #include "mysql_constructed_resultset.h"
 #include "mysql_cset_metadata.h"
 
