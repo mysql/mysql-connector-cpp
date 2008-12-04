355a3d6818e382e48b11ced2b0e2db366112a0fe
Ulf Wendel <nixnutz@ulflinux>
2008-12-04 11:37:40 +0100

    Adding examples with a formatting suitable for the docs team

---
+++ examples/standalone_example_docs1.cpp
@@ -0,0 +1,82 @@
+/* Copyright 2008 Sun Microsystems, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   There are special exceptions to the terms and conditions of the GPL
+   as it is applied to this software. View the full text of the
+   exception in file EXCEPTIONS-CONNECTOR-C++ in the directory of this
+   software distribution.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+/* Standard C++ includes */
+#include <stdlib.h>
+#include <iostream>
+
+/*
+  Include directly the different
+  headers from cppconn/ and mysql_driver.h + mysql_util.h
+  (and mysql_connection.h). This will reduce your build time!
+*/
+#include "mysql_connection.h"
+
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
+
+using namespace std;
+
+int main(void)
+{
+cout << endl;
+cout << "Running 'SELECT 'Hello World!' AS _message'..." << endl;
+
+try {
+  sql::Driver *driver;
+  sql::Connection *con;
+  sql::Statement *stmt;
+  sql::ResultSet *res;
+
+  /* Create a connection */
+  driver = get_driver_instance();
+  con = driver->connect("tcp://127.0.0.1:3306", "root", "root");
+  /* Connect to the MySQL test database */
+  con->setSchema("test");
+
+  stmt = con->createStatement();
+  res = stmt->executeQuery("SELECT 'Hello World!' AS _message");
+  while (res->next()) {
+    cout << "\t... MySQL replies: ";
+    /* Access column data by alias or column name */
+    cout << res->getString("_message") << endl;
+    cout << "\t... MySQL says it again: ";
+    /* Access column fata by numeric offset, 1 is the first column */
+    cout << res->getString(1) << endl;
+  }
+  delete res;
+  delete stmt;
+  delete con;
+
+} catch (sql::SQLException &e) {
+  cout << "# ERR: SQLException in " << __FILE__;
+  cout << "(" << __FUNCTION__ << ") on line " << __LINE__ << endl;
+  cout << "# ERR: " << e.what();
+  cout << " (MySQL error code: " << e.getErrorCode();
+  cout << ", SQLState: " << e.getSQLState() << " )" << endl;
+}
+
+cout << endl;
+
+return EXIT_SUCCESS;
+}
