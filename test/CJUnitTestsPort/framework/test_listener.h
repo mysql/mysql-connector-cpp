ff7be6c5d19756eb63c79571b6e7d8595c727896
Ulf Wendel <nixnutz@ulflinux>
2008-12-16 10:36:01 +0100

    Might fix build on linux-rhel3-x86_64 et al.

--- test/CJUnitTestsPort/framework/test_listener.h
+++ test/CJUnitTestsPort/framework/test_listener.h
@@ -27,6 +27,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include <vector>
 
 #include "start_options.h"
+#include "test_tapOutputter.h"
 #include <cppconn/exception.h>
 #include "../common/stringutils.h"
 #include "../common/singleton.h"
