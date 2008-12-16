b55f35e6fbbc9912047e37801eb459304e899d74
Ulf Wendel <nixnutz@ulflinux>
2008-12-16 15:14:52 +0100

    Finished documenting example unit test

--- test/CJUnitTestsPort/example_base.cpp
+++ test/CJUnitTestsPort/example_base.cpp
@@ -7,7 +7,7 @@
 
    There are special exceptions to the terms and conditions of the GPL
    as it is applied to this software. View the full text of the
-   exception in file EXCEPTIONS-CONNECTOR-C++ in the directory of this
+   exception in file EXCEPTIONS-conECTOR-C++ in the directory of this
    software distribution.
 
    This program is distributed in the hope that it will be useful,
@@ -28,24 +28,34 @@
 namespace testsuite
 {
 
-	static const String default_url = "tcp://127.0.0.1";
-	static const String default_db = "test";
-	static const String default_user =  "root";
-	static const String default_passwd = "root";
-	
-Driver * example_fixture::driver=NULL;
+/**
+ * Fallback connection URL if no URL is passed to the test on the command line
+ */
+static const String default_url="tcp://127.0.0.1";
 
 /**
- * Creates a new example_fixture object.
- *
- * @param name The name of the unit test case
+ * Fallback connection schema to use if no schema is passed to the test on the command line
  */
+static const String default_db="test";
+
+/**
+ * Fallback user name if no schema is passed to the test on the command line
+ */
+static const String default_user="root";
+
+/**
+ * Fallback user password if no schema is passwrd to the test on the command line
+ */
+static const String default_passwd="root";
+
+Driver * example_fixture::driver=NULL;
+
 example_fixture::example_fixture(const String & name)
 : super(name),
-conn(NULL),
+con(NULL),
 pstmt(NULL),
 stmt(NULL),
-rs(NULL)
+res(NULL)
 {
   init();
 }
@@ -58,196 +68,127 @@ void example_fixture::init()
   db=TestsRunner::theInstance().getStartOptions()->dbSchema;
 }
 
-/**
- * Creates resources used by all tests.
- *
- * @throws Exception
- *             if an error occurs.
- */
 void example_fixture::setUp()
 {
-  this->created_objects.clear();
+  created_objects.clear();
 
   try
   {
-    this->conn.reset(getConnection());
+    con.reset(getConnection());
   } catch (sql::SQLException & sqle)
   {
     logErr(String("Couldn't get connection") + sqle.what());
     throw sqle;
   }
 
-  this->stmt.reset(this->conn->createStatement());
+  this->stmt.reset(this->con->createStatement());
 
-	/* TODO: connect message incl. version using logDebug() */
-  
-	DatabaseMetaData dbmd(conn->getMetaData());
-  stmt->execute(String("USE ") + (db.length() > 0 ? db : default_db));
+  /* TODO: conect message incl. version using logDebug() */
+  con->setSchema((db.length() > 0 ? db : default_db));
 }
 
-/**
- * Destroys SQL schema objects created during the test case.
- *
- * @throws Exception
- */
 void example_fixture::tearDown()
 {
-	
-  rs.reset();
-  for (int i=0; i < static_cast<int> (this->created_objects.size() - 1); i+=2)
+
+  res.reset();
+  for (int i=0; i < static_cast<int> (created_objects.size() - 1); i+=2)
   {
     try
     {
-      dropSchemaObject(this->created_objects[ i ], this->created_objects[ i + 1 ]);
-    } catch (sql::SQLException &) { }
+      dropSchemaObject(created_objects[ i ], created_objects[ i + 1 ]);
+    } catch (sql::SQLException &)
+    {
+    }
   }
 
   stmt.reset();
   pstmt.reset();
-  conn.reset();
+  con.reset();
 }
 
-/* throws SQLException & */
-
-void example_fixture::createSchemaObject(String objectType, String objectName,
-                                         String columnsAndOtherStuff)
+void example_fixture::createSchemaObject(String object_type, String object_name,
+                                         String columns_and_other_stuff)
 {
-  this->created_objects.push_back(objectType);
-  this->created_objects.push_back(objectName);
+  created_objects.push_back(object_type);
+  created_objects.push_back(object_name);
 
-  dropSchemaObject(objectType, objectName);
-  String sql(_T("CREATE  "));
+  dropSchemaObject(object_type, object_name);
 
-  sql.resize(objectName.length()
-                   + objectType.length() + columnsAndOtherStuff.length() + 10);
-
-  sql.append(objectType);
+  String sql("CREATE  ");
+  sql.resize(object_name.length()
+             + object_type.length() + columns_and_other_stuff.length() + 10);
+  sql.append(object_type);
   sql.append(" ");
-  sql.append(objectName);
+  sql.append(object_name);
   sql.append(" ");
-  sql.append(columnsAndOtherStuff);
+  sql.append(columns_and_other_stuff);
+
   this->stmt->executeUpdate(sql);
 }
 
-void example_fixture::dropSchemaObject(String objectType, String objectName)
+void example_fixture::dropSchemaObject(String object_type, String object_name)
 {
-  this->stmt->executeUpdate(String("DROP ") + objectType + " IF EXISTS "
-                            + objectName);
+  this->stmt->executeUpdate(String("DROP ") + object_type + " IF EXISTS "
+                            + object_name);
 }
 
-
-void example_fixture::createTable(String tableName, String columnsAndOtherStuff)
+void example_fixture::createTable(String table_name, String columns_and_other_stuff)
 {
-  createSchemaObject("TABLE", tableName, columnsAndOtherStuff);
+  createSchemaObject("TABLE", table_name, columns_and_other_stuff);
 }
 
 /* throws SQLException & */
 
-void example_fixture::dropTable(String tableName)
+void example_fixture::dropTable(String table_name)
 {
-  dropSchemaObject("TABLE", tableName);
+  dropSchemaObject("TABLE", table_name);
 }
 
-/**
- * Returns a database connection
- *
- * @throws SQLException &
- */
 sql::Connection * example_fixture::getConnection()
 {
   if (driver == NULL)
   {
     driver=get_driver_instance();
-    logMsg(String(_T("Done: loaded ")) + driver->getName());
+    logMsg(String("Done: loaded ") + driver->getName());
   }
 
-  if (url.length() == 0)  
+  if (url.length() == 0)
     url=default_url;
-	
-	if (user.length() == 0)
-		user = default_user;
-	
-	if (passwd.length() == 0)
-		passwd = default_passwd;
+
+  if (user.length() == 0)
+    user=default_user;
+
+  if (passwd.length() == 0)
+    passwd=default_passwd;
 
   return driver->connect(url, user, passwd);
 }
 
-
-
-
-/**
- * Checks whether the database we're connected to meets the given version
- * minimum
- *
- * @param major the major version to meet
- * @param minor the minor version to meet 
- * @return bool if the major/minor is met
- * @throws SQLException& if an error occurs.
- */
 bool example_fixture::versionMeetsMinimum(int major, int minor)
 {
   return versionMeetsMinimum(major, minor, 0);
 }
 
-/**
- * Checks whether the database we're connected to meets the given version
- * minimum
- *
- * @param major the major version to meet
- * @param minor the minor version to meet
- * @return bool if the major/minor is met
- * @throws SQLException&
- */
 bool example_fixture::versionMeetsMinimum(int major, int minor, int subminor)
 {
   return true;
   /*
-  ((dynamic_cast<sql::mysql::MySQL_Connection*> (this->conn)->versionMeetsMinimum(
+  ((dynamic_cast<sql::mysql::MySQL_Connection*> (this->con)->versionMeetsMinimum(
   major, minor, subminor));*/
 
 }
 
-void example_fixture::closeMemberJDBCResources()
-{
-  if (this->rs.get() != NULL)
-  {
-    ResultSet toClose=this->rs;
-    this->rs.reset();
-
-    try
-    {
-      toClose->close();
-    } catch (sql::SQLException &)
-    {
-      // ignore
-    }
-  }
-  if (this->pstmt.get() != NULL)
-  {
-    PreparedStatement toClose=this->pstmt;
-    this->pstmt.reset();
-    try
-    {
-      toClose->close();
-    } catch (sql::SQLException &)
-    {
-      // ignore
-    }
-  }
-}
-
-void example_fixture::logMsg(String message)
+void example_fixture::logMsg(const String message)
 {
   TestsListener::theInstance().messagesLog() << message << std::endl;
 }
 
-void example_fixture::logErr(String message)
+void example_fixture::logErr(const String message)
 {
   TestsListener::theInstance().errorsLog() << message << std::endl;
 }
 
-void example_fixture::logDebug(const String & message)
+void example_fixture::logDebug(const String message)
 {
   logMsg(message);
 }
--- test/CJUnitTestsPort/example_base.h
+++ test/CJUnitTestsPort/example_base.h
@@ -7,7 +7,7 @@ the Free Software Foundation; version 2 of the License.
 
 There are special exceptions to the terms and conditions of the GPL
 as it is applied to this software. View the full text of the
-exception in file EXCEPTIONS-CONNECTOR-C++ in the directory of this
+exception in file EXCEPTIONS-conECTOR-C++ in the directory of this
 software distribution.
 
 This program is distributed in the hope that it will be useful,
@@ -34,6 +34,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 namespace testsuite
 {
+/* TODO - document */
 typedef std::auto_ptr<sql::Connection> Connection;
 typedef std::auto_ptr<sql::PreparedStatement> PreparedStatement;
 typedef std::auto_ptr<sql::Statement> Statement;
@@ -44,150 +45,217 @@ typedef std::auto_ptr<sql::DatabaseMetaData> DatabaseMetaData;
 
 class example_fixture : public TestSuite
 {
-		
-public:	
+private:
   typedef TestSuite super;
 
-	/**
-	 * List of SQL schema objects created during the test run
-	 *
-	 * Used by tearDown() to clean up the database after the test run
-	 */
+  /**
+   * List of SQL schema objects created during the test run
+   *
+   * Used by tearDown() to clean up the database after the test run
+   */
   List created_objects;
-	
-	/**
-	 * Driver to be used
-	 *
-	 */
+
+  /**
+   * Driver to be used
+   *
+   * The element is managed by getConnection(). You do not need
+   * to worry about the creation of a connection or a driver object,
+   * use getConnection() to obtain a connection object.
+   */
   static Driver *driver;
-	
-  static void logMsg(String message);
-  void logErr(String message);
+
+  /**
+   * Write (info) message to test protocol
+   *
+   * @param	message	Message to be included in the protocol
+   */
+  void logMsg(const String message);
+
+  /**
+   * Write error to test protocol
+   *
+   * @param	message Message to be included in the protocol
+   */
+  void logErr(const String message);
 
 protected:
 
-	/**
-	 * 
-	 *
-	 *
-	 */
+  /**
+   * Copies command line connection parameter into protected slots
+   *
+   */
   void init();
-		
-	String url;
-	String db;
-	String user;
-	String passwd;
-	
-
-
-  /** Connection to server, initialized in setUp() Cleaned up in tearDown(). */
-
-  Connection conn;
-
-  /** The driver to use */
-
-  String dbClass;
 
   /**
-   * PreparedStatement to be used in tests, not initialized. Cleaned up in
-   * tearDown().
+   * Effective database connection URL
+   *
+   * URL refers to the conector/C++ connection URL syntax
+   *
+   * Fallback to default_url, see also default_url
+   */
+  String url;
+
+  /**
+   * Name of the schema to use (USE <db>)
+   *
+   * The schema will be selected during setUp() before running a test
+   * Fallback to default_db, see also default_db
+   */
+  String db;
+
+  /**
+   * Database user for getConnection()
+   *
+   * Fallback to default_user, see also default_user
+   */
+  String user;
+
+  /**
+   * Database user password
+   *
+   * Fallback to default_password, see also default_password
+   */
+  String passwd;
+
+  /**
+   * Database connection, initiated during setUp() and cleaned up in tearDown()
+   *
+   */
+  Connection con;
+
+  /**
+   * PreparedStatement to be used in tests, not initialized. Cleaned up in tearDown()
    */
   PreparedStatement pstmt;
 
   /**
-   * Statement to be used in tests, initialized in setUp(). Cleaned up in
-   * tearDown().
+   * Statement to be used in tests, not initialized. Cleaned up in tearDown()
    */
   Statement stmt;
 
   /**
-   * ResultSet to be used in tests, not initialized. Cleaned up in tearDown().
+   * ResultSet to be used in tests, not initialized. Cleaned up in tearDown()
    */
-  ResultSet rs;
-  /* throws SQLException */
+  ResultSet res;
 
-  void createSchemaObject(String objectType, String objectName,
-                          String columnsAndOtherStuff);
-	void dropSchemaObject(String objectType, String objectName);
+  /**
+   * Creates a SQL schema object
+   *
+   * Use this method to create arbitrary SQL object if you want the test
+   * framework to clean up SQL schema objects for you at the end of the test
+   * in tearDown(). The framework will register all objects created with any
+   * of the createXYZ() methods and call the appropriate dropXYZ() method during
+   * tearDown().
+   *
+   * Although this method is protected and available in the test, you should
+   * prefer calling any of the specialized wrappers, for example createTable().
+   *
+   * @param	object_type							SQL type, e.g. TABLE, FUNCTION, ...
+   * @param object_name							SQL element name, e.g. "table1"
+   * @param	columns_and_other_stuff Additional SQL definitions
+   * @throws SQLException*
+   */
 
-  void createTable(String tableName, String columnsAndOtherStuff);
-  void dropTable(String tableName);
+  void createSchemaObject(String object_type, String object_name,
+                          String columns_and_other_stuff);
 
+  /**
+   * Drops a SQL schema object
+   *
+   * All SQL objects created by createSchemaObject() will be implicitly
+   * dropped during tearDown().
+   *
+   * @param object_type			SQL type e.g. TABLE, FUNCTION, ...
+   * @param object_name			SQL element name, e.g. "table1"
+   * @throws SQLException*
+   */
+  void dropSchemaObject(String object_type, String object_name);
+
+  /**
+   * Create a SQL table of the given name and with the specified SQL definition
+   *
+   * @param table_name							SQL table name
+   * @param	columns_and_other_stuff Additional SQL definitions
+   * @throws SQLException*
+   */
+  void createTable(String table_name, String columns_and_other_stuff);
+
+  /**
+   * Drops a SQL table
+   *
+   * Note: all SQL tables created by createTable() will implicitly dropped
+   * during tearDown().
+   *
+   * @param	table_name	SQL table name
+   * @throws SQLException*
+   */
+  void dropTable(String table_name);
+
+  /**
+   * Returns a Connector/C++ connection object
+   *
+   * Note: you are responsible for handling the object, you might
+   * want to use typedef std::auto_ptr<sql::Connection>con(getConnection())
+   * or similar for convenience reasons.
+   *
+   * @throws SQLException*
+   */
   sql::Connection * getConnection();
 
-  
+
   /**
-   * Checks whether the database we're connected to meets the given version
-   * minimum
+   * Checks whether the database we're conected to meets the given version minimum
    *
-   * @param major
-   *            the major version to meet
-   * @param minor
-   *            the minor version to meet
-   *
+   * @param major the major version to meet
+   * @param minor the minor version to meet
    * @return boolean if the major/minor is met
-   *
-   * @throws SQLException
-   *             if an error occurs.
+   * @throws SQLException*
    */
-
-  /* throws SQLException */
-
   bool versionMeetsMinimum(int major, int minor);
+
   /**
-   * Checks whether the database we're connected to meets the given version
-   * minimum
+   * Checks whether the database we're conected to meets the given version minimum
    *
-   * @param major
-   *            the major version to meet
-   * @param minor
-   *            the minor version to meet
-   *
+   * @param major the major version to meet
+   * @param minor the minor version to meet
+   * @param subminor the subminor version to meet
    * @return boolean if the major/minor is met
-   *
-   * @throws SQLException
-   *             if an error occurs.
+   * @throws SQLException*
    */
-
-  /* throws SQLException */
-
-  bool versionMeetsMinimum(int major
-                           , int minor
-                           , int subminor);
-
-  void closeMemberJDBCResources();
+  bool versionMeetsMinimum(int major, int minor, int subminor);
 
 public:
 
   /**
    * Creates a new BaseTestCase object.
    *
-   * @param name
-   *            The name of the JUnit test case
+   * @param name The name of the unit test case
    */
-
   example_fixture(const String & name);
 
-  void logDebug(const String & message);
+  /**
+   * Writes a message to the debug log
+   *
+   * @param message
+   */
+  void logDebug(const String message);
+
   /**
    * Creates resources used by all tests.
    *
-   * @throws Exception
-   *             if an error occurs.
+   * TODO: which ones?
+   *
+   * @throws SQLException*
    */
-
-  /* throws Exception */
-
   virtual void setUp();
+
   /**
    * Destroys resources created during the test case.
    *
-   * @throws Exception
-   *             DOCUMENT ME!
+   * @throws SQLException*
    */
-
-  /* throws Exception */
   virtual void tearDown();
+
 };
 
 } /* namespace testsuite */
