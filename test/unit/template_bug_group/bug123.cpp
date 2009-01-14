74e2a47ed8d1a03f08cb9fc39da88c914186c0c1
Ulf Wendel <nixnutz@ulflinux>
2009-01-14 18:10:15 +0100

    Adding template for bug regression test, pimping README

---
+++ test/unit/template_bug_group/bug123.cpp
@@ -0,0 +1,69 @@
+/*
+Copyright 2008 Sun Microsystems, Inc.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 2 of the License.
+
+There are special exceptions to the terms and conditions of the GPL
+as it is applied to this software. View the full text of the
+exception in file EXCEPTIONS-CONNECTOR-C++ in the directory of this
+software distribution.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include "bug123.h"
+
+namespace testsuite
+{
+namespace regression
+{
+
+void template_bug123_class::template_bug123_method()
+{
+  logMsg("Regression test for #[TODO - add bug numer]");
+  try
+  {
+    /*
+     By default the framework will establish a connection in setUp() and connect
+     to the configured MySQL Server and select the configured schema.
+     No other members will be initialized, however, there are several
+     auto-ptr members which you can make use of: this->stmt, this->pstmt, this->res.
+     The good thing about the auto-ptr members is that you don't need to care much
+     about them. tearDown() will reset them and auto-ptr will ensure proper
+     memory management.
+     */
+    stmt.reset(con->createStatement());
+
+    /* Running a SELECT and storing the returned result set in this->res */
+    res.reset(stmt->executeQuery("SELECT 'Hello world!'"));
+
+    /* Move result set cursor to first rowm, fetch result, write result to log  */
+    res->next();
+    logMsg(res->getString(1));
+
+  } catch (sql::SQLException &e)
+  {
+    /* If anything goes wrong, write some info to the log... */
+    logErr(e.what());
+    logErr("SQLState: " + e.getSQLState());
+
+    /*
+     ... and let the test fail. FAIL() is a macro.
+     FAIL calls fail(const char* reason, const char* file, int line)
+     */
+    FAIL(e.what());
+  }
+  /* If all goes fine, there is no need to call PASS() or something.	 */
+}
+
+} /* namespace regression */
+} /* namespace testsuite */
