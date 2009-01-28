4cd59a5177ae75ad336409794c5fa4e5bedec8cf
lawrin <lawrin@dad>
2009-01-28 17:17:14 +0200

    fixed header inclusions in the tests framework

--- test/CJUnitTestsPort/framework/test_runner.cpp
+++ test/CJUnitTestsPort/framework/test_runner.cpp
@@ -21,6 +21,8 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 #include "test_runner.h"
 #include "test_factory.h"
+#include "test_case.h"
+#include "test_listener.h"
 
 namespace testsuite
 {
