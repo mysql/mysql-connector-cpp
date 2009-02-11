0f3b24aae5cec70b663cd5edbfee4c2dabb17866
Ulf Wendel <nixnutz@ulflinux>
2009-02-11 16:41:29 +0100

    Letting the test fail by following JDBC specs word by word.

--- test/unit/classes/connectionmetadata.cpp
+++ test/unit/classes/connectionmetadata.cpp
@@ -35,9 +35,68 @@ namespace classes
 
 void connectionmetadata::init()
 {
+  /*
+   ResultSet getAttributes(String catalog,
+                        String schemaPattern,
+                        String typeNamePattern,
+                        String attributeNamePattern)
+                        throws SQLException
+
+   Retrieves a description of the given attribute of the given type for a user-defined type (UDT) that is available in the given schema and catalog.
+
+Descriptions are returned only for attributes of UDTs matching the catalog, schema, type, and attribute name criteria. They are ordered by TYPE_CAT, TYPE_SCHEM, TYPE_NAME and ORDINAL_POSITION. This description does not contain inherited attributes.
+
+The ResultSet object that is returned has the following columns:
+
+   1. TYPE_CAT String => type catalog (may be null)
+   2. TYPE_SCHEM String => type schema (may be null)
+   3. TYPE_NAME String => type name
+   4. ATTR_NAME String => attribute name
+   5. DATA_TYPE int => attribute type SQL type from java.sql.Types
+   6. ATTR_TYPE_NAME String => Data source dependent type name. For a UDT, the type name is fully qualified. For a REF, the type name is fully qualified and represents the target type of the reference type.
+   7. ATTR_SIZE int => column size. For char or date types this is the maximum number of characters; for numeric or decimal types this is precision.
+   8. DECIMAL_DIGITS int => the number of fractional digits. Null is returned for data types where DECIMAL_DIGITS is not applicable.
+   9. NUM_PREC_RADIX int => Radix (typically either 10 or 2)
+  10. NULLABLE int => whether NULL is allowed
+   * attributeNoNulls - might not allow NULL values
+   * attributeNullable - definitely allows NULL values
+   * attributeNullableUnknown - nullability unknown
+  11. REMARKS String => comment describing column (may be null)
+  12. ATTR_DEF String => default value (may be null)
+  13. SQL_DATA_TYPE int => unused
+  14. SQL_DATETIME_SUB int => unused
+  15. CHAR_OCTET_LENGTH int => for char types the maximum number of bytes in the column
+  16. ORDINAL_POSITION int => index of the attribute in the UDT (starting at 1)
+  17. IS_NULLABLE String => ISO rules are used to determine the nullability for a attribute.
+   * YES --- if the attribute can include NULLs
+   * NO --- if the attribute cannot include NULLs
+   * empty string --- if the nullability for the attribute is unknown
+  18. SCOPE_CATALOG String => catalog of table that is the scope of a reference attribute (null if DATA_TYPE isn't REF)
+  19. SCOPE_SCHEMA String => schema of table that is the scope of a reference attribute (null if DATA_TYPE isn't REF)
+  20. SCOPE_TABLE String => table name that is the scope of a reference attribute (null if the DATA_TYPE isn't REF)
+  21. SOURCE_DATA_TYPE short => source type of a distinct type or user-generated Ref type,SQL type from java.sql.Types (null if DATA_TYPE isn't DISTINCT or user-generated REF)
+   */
   attributes.push_back(udtattribute("TYPE_CAT", 0));
   attributes.push_back(udtattribute("TYPE_SCHEM", 0));
   attributes.push_back(udtattribute("TYPE_NAME", 0));
+  attributes.push_back(udtattribute("ATTR_NAME", 0));
+  attributes.push_back(udtattribute("DATA_TYPE", 0));
+  attributes.push_back(udtattribute("ATTR_TYPE_NAME", 0));
+  attributes.push_back(udtattribute("ATTR_SIZE", 0));
+  attributes.push_back(udtattribute("DECIMAL_DIGITS", 0));
+  attributes.push_back(udtattribute("NUM_PREC_RADIX", 0));
+  attributes.push_back(udtattribute("NULLABLE", 0));
+  attributes.push_back(udtattribute("REMARKS", 0));
+  attributes.push_back(udtattribute("ATTR_DEF", 0));
+  attributes.push_back(udtattribute("SQL_DATA_TYPE", 0));
+  attributes.push_back(udtattribute("SQL_DATETIME_SUB", 0));
+  attributes.push_back(udtattribute("CHAR_OCTET_LENGTH", 0));
+  attributes.push_back(udtattribute("ORDINAL_POSITION", 0));
+  attributes.push_back(udtattribute("IS_NULLABLE", 0));
+  attributes.push_back(udtattribute("SCOPE_CATALOG", 0));
+  attributes.push_back(udtattribute("SCOPE_SCHEMA", 0));
+  attributes.push_back(udtattribute("SCOPE_TABLE", 0));
+  attributes.push_back(udtattribute("SOURCE_DATA_TYPE", 0));
 }
 
 void connectionmetadata::getSchemata()
@@ -103,7 +162,7 @@ void connectionmetadata::getAttributes()
       msg.str("");
       msg << "... OK found column " << it->name;
       logMsg(msg.str());
-      
+
       it++;
     }
     if (it != attributes.end())
