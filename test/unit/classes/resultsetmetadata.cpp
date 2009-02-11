87153a7e314e463b743de30148a991b5fe9b35a4
andrey.hristov@sun.com <>
2009-02-11 19:00:28 +0200

    Type YEAR removed, it should be DATE

--- test/unit/classes/resultsetmetadata.cpp
+++ test/unit/classes/resultsetmetadata.cpp
@@ -73,9 +73,9 @@ void resultsetmetadata::init()
   columns.push_back(columndefinition("DATETIME", "DATETIME", sql::DataType::TIMESTAMP, "2009-02-09 20:05:43", true));
   columns.push_back(columndefinition("TIMESTAMP", "TIMESTAMP", sql::DataType::TIMESTAMP, "2038-01-09 03:14:07", true));
   columns.push_back(columndefinition("TIME", "TIME", sql::DataType::TIME, "-838:59:59", true, true));
-  columns.push_back(columndefinition("YEAR", "YEAR", sql::DataType::YEAR, "1901", true));
-  columns.push_back(columndefinition("YEAR", "YEAR(4)", sql::DataType::YEAR, "2009", false));
-  columns.push_back(columndefinition("YEAR", "YEAR(2)", sql::DataType::YEAR, "1", false));
+  columns.push_back(columndefinition("YEAR", "YEAR", sql::DataType::DATE, "1901", true));
+  columns.push_back(columndefinition("YEAR", "YEAR(4)", sql::DataType::DATE, "2009", false));
+  columns.push_back(columndefinition("YEAR", "YEAR(2)", sql::DataType::DATE, "1", false));
   columns.push_back(columndefinition("CHAR", "CHAR", sql::DataType::CHAR, "a", true));
   columns.push_back(columndefinition("CHAR", "CHAR(255)", sql::DataType::CHAR, "abc", false));
   columns.push_back(columndefinition("CHAR", "NATIONAL CHAR(255)", sql::DataType::CHAR, "abc", false));
