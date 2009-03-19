370299bb14b037c9be5ca9ee11fc64c185fdd8cd
andrey.hristov@sun.com <>
2009-03-19 18:06:53 +0100

    re-phrase so HPUX is happy, prolly

--- driver/mysql_util.h
+++ driver/mysql_util.h
@@ -36,19 +36,23 @@
 #  ifndef HAVE_STRTOLL
 #    ifdef HAVE_STRTOL
 #      define strtoll(__a, __b, __c) strtol((__a), (__b), (__c))
-#    elif defined(HAVE_STRTOIMAX)
-#      define strtoll(__a, __b, __c) strtoimax((__a), NULL, 10)
 #    else
-#error "Compilation will fail because code does not know an equivalent of strtol/strtoll"
+#      ifdef HAVE_STRTOIMAX
+#        define strtoll(__a, __b, __c) strtoimax((__a), NULL, 10)
+#      else
+#        error "Compilation will fail because code does not know an equivalent of strtol/strtoll"
+#      endif
 #    endif
 #  endif
 #  ifndef HAVE_STRTOULL
 #    ifdef HAVE_STRTOUL
 #      define strtoull(__a, __b, __c) strtoul((__a), (__b), (__c))
-#    elif defined(HAVE_STRTOUMAX)
-#      define strtoull(__a, __b, __c) strtoumax((__a), NULL, 10)
 #    else
-#      error Compilation will fail because code does not know an equivalent of strtoul/strtoull
+#      ifdef HAVE_STRTOUMAX
+#        define strtoull(__a, __b, __c) strtoumax((__a), NULL, 10)
+#      else
+#        error Compilation will fail because code does not know an equivalent of strtoul/strtoull
+#      endif
 #    endif
 #  endif
 #  ifdef __hpux
