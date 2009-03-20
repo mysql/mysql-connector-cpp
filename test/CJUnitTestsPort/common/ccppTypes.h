05442ce5893c2e27f401836bb80708860b8a9cdc
andrey.hristov@sun.com <>
2009-03-20 09:18:55 +0100

    commit forgotten piece of code

--- test/CJUnitTestsPort/common/ccppTypes.h
+++ test/CJUnitTestsPort/common/ccppTypes.h
@@ -26,6 +26,36 @@
 #endif
 
 #include "cppconn/config.h"
+#ifndef _WIN32
+#  include <stdlib.h>
+#  ifndef HAVE_STRTOLL
+#    ifdef HAVE_STRTOL
+#      define strtoll(__a, __b, __c) strtol((__a), (__b), (__c))
+#    else
+#      ifdef HAVE_STRTOIMAX
+#        define strtoll(__a, __b, __c) strtoimax((__a), NULL, 10)
+#      else
+#        error "Compilation will fail because code does not know an equivalent of strtol/strtoll"
+#      endif
+#    endif
+#    define HAVE_STRTOLL 1
+#  endif
+#  ifndef HAVE_STRTOULL
+#    ifdef HAVE_STRTOUL
+#      define strtoull(__a, __b, __c) strtoul((__a), (__b), (__c))
+#    else
+#      ifdef HAVE_STRTOUMAX
+#        define strtoull(__a, __b, __c) strtoumax((__a), NULL, 10)
+#      else
+#        error Compilation will fail because code does not know an equivalent of strtoul/strtoull
+#      endif
+#    endif
+#    define HAVE_STRTOULL 1
+#  endif
+#else
+#  define strtoll(x, e, b) _strtoi64((x), (e), (b))
+#  define strtoull(x, e, b) _strtoui64((x), (e), (b))
+#endif	//	_WIN32
 
 
 #include <vector>
