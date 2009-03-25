70ed71931daf2d562cf673f6d6e4d26fcc737e16
lawrin <lawrin@dad>
2009-03-25 20:40:27 +0200

    1). applied georg's diff (Intel/winx64 in name) 2). Redistributables are not a feature 3). Removed new win warnings 4). Added messages about found msvc* dependencies for installer

--- driver/mysql_ps_resultset.cpp
+++ driver/mysql_ps_resultset.cpp
@@ -353,16 +353,16 @@ MySQL_Prepared_ResultSet::getDouble(const uint32_t columnIndex) const
 		case sql::DataType::BIGINT:
 		{
 			long double ret;
-			bool is_it_unsigned = stmt->bind[columnIndex - 1].is_unsigned;
+			bool is_it_unsigned = stmt->bind[columnIndex - 1].is_unsigned != 0;
 			CPP_INFO_FMT("It's an int : %ssigned", is_it_unsigned? "un":"");
 			if (is_it_unsigned) {
 				uint64_t ival = getUInt64_intern(columnIndex, false);
 				CPP_INFO_FMT("value=%llu", ival);
-				ret = ival;
+				ret = static_cast<long double>(ival);
 			} else {
 				int64_t ival = getInt64_intern(columnIndex, false);
 				CPP_INFO_FMT("value=%lld", ival);
-				ret = ival;
+				ret = static_cast<long double>(ival);
 			}
 			CPP_INFO_FMT("value=%10.10f", (double) ret);
 			return ret;
