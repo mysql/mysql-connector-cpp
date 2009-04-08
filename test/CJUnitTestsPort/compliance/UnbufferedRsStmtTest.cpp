adbff72f591da4170eec3415cbb7ee627300e66b
lawrin <lawrin@dad>
2009-04-08 17:48:06 +0300

    added statement tests w/ not buffered resultsets (extended a bit complience::StatementTest, so the option is set in setUp) uncommented 2 tests there

---
+++ test/CJUnitTestsPort/compliance/UnbufferedRsStmtTest.cpp
@@ -0,0 +1,38 @@
+/*
+   Copyright (C) 2007 - 2008 MySQL AB, 2008 - 2009 Sun Microsystems, Inc.
+
+   The MySQL Connector/C++ is licensed under the terms of the GPL
+   <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>, like most
+   MySQL Connectors. There are special exceptions to the terms and
+   conditions of the GPL as it is applied to this software, see the
+   FLOSS License Exception
+   <http://www.mysql.com/about/legal/licensing/foss-exception.html>.
+ */
+
+#include "UnbufferedRsStmtTest.h"
+
+
+namespace testsuite
+{
+namespace compliance
+{
+
+  UnbufferedRsStmtTest::UnbufferedRsStmtTest( const String & name ) : StatementTest( name )
+  {
+    logMsg( "UnbufferedRsStmtTest ctor called" );
+  }
+
+  void UnbufferedRsStmtTest::setUp()
+  {
+    logMsg( "UnbufferedRsStmtTest setUp called" );
+
+    super::setUp();
+
+    sql::ResultSet::enum_type unbuffered= sql::ResultSet::TYPE_FORWARD_ONLY;
+    conn->setClientOption( "defaultStatementResultType", & unbuffered );
+
+    stmt->setResultSetType( unbuffered );
+  }
+
+}
+}
---
+++ test/CJUnitTestsPort/compliance/UnbufferedRsStmtTest.h
@@ -0,0 +1,40 @@
+/*
+   Copyright (C) 2007 - 2008 MySQL AB, 2008 - 2009 Sun Microsystems, Inc.
+
+   The MySQL Connector/C++ is licensed under the terms of the GPL
+   <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>, like most
+   MySQL Connectors. There are special exceptions to the terms and
+   conditions of the GPL as it is applied to this software, see the
+   FLOSS License Exception
+   <http://www.mysql.com/about/legal/licensing/foss-exception.html>.
+ */
+
+#include "StatementTest.h"
+
+
+namespace testsuite
+{
+namespace compliance
+{
+
+  // Same testsuite, as Statement test
+class UnbufferedRsStmtTest : public StatementTest
+{
+private:
+  typedef StatementTest super;
+
+protected:
+
+public:
+
+  typedef UnbufferedRsStmtTest TestSuiteClass;
+  UnbufferedRsStmtTest( const String & name );
+
+  virtual void setUp();
+
+};
+
+REGISTER_FIXTURE(UnbufferedRsStmtTest);
+
+}
+}
