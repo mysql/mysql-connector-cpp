e6a709175a8b18db29defa6ae0bd24330e67c1ea
Andrey Hristov <andrey.hristov@sun.com>
2010-07-02 11:06:19 +0200

    Check return value of mysql_stmt_store_result and if an error throw an exception.

--- driver/mysql_prepared_statement.cpp
+++ driver/mysql_prepared_statement.cpp
@@ -314,7 +314,9 @@ MySQL_Prepared_Statement::executeQuery()
 	proxy->attr_set( STMT_ATTR_UPDATE_MAX_LENGTH, &bool_tmp);
 	sql::ResultSet::enum_type tmp_type;
 	if (resultset_type == sql::ResultSet::TYPE_SCROLL_INSENSITIVE) {
-		proxy->store_result();
+		if (proxy->store_result()) {
+			sql::mysql::util::throwSQLException(*proxy.get());		
+		}
 		tmp_type = sql::ResultSet::TYPE_SCROLL_INSENSITIVE;
 	} else if (resultset_type == sql::ResultSet::TYPE_FORWARD_ONLY) {
 		tmp_type = sql::ResultSet::TYPE_FORWARD_ONLY;
