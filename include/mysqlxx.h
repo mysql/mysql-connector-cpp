fce17691a1de4bb270c694c1edbdabd2a526b34f
Rafal Somla <rafal.somla@oracle.com>
2015-07-29 15:22:30 +0200

    Handling result sets - first attempts.

--- include/mysqlxx.h
+++ include/mysqlxx.h
@@ -3,28 +3,56 @@
 
 #include <mysql/cdk.h>
 
+#include <vector>
 #include <string>
 #include <ostream>
 
 namespace mysqlx {
 
+using cdk::foundation::nocopy;
+
+using cdk::byte;
 using cdk::string;
 using std::ostream;
 typedef unsigned col_count_t;
 
-class Session
+class Result;
+class Row;
+
+class Session : nocopy
 {
+protected:
+
   cdk::ds::TCPIP  m_ds;
   std::string     m_pwd;
   cdk::Session    m_sess;
 
+  class Access : private cdk::Reply
+  {
+    typedef cdk::Reply::Initializer Initializer;
+    friend class Session;
+  };
+
+  typedef Access::Initializer Result_init;
+
 public:
+
+  Session(const char *host, unsigned short port,
+          const string  &user,
+          const char    *pwd =NULL)
+  : m_ds(host, port)
+  , m_pwd(pwd ? pwd : "")
+  , m_sess(m_ds, cdk::ds::Options(user, pwd ? &m_pwd : NULL))
+  {
+    if (!m_sess.is_valid())
+      throw m_sess.get_error();
+  }
 
   Session(unsigned short port,
           const string  &user,
           const char    *pwd =NULL)
   : m_ds("localhost", port)
-  , m_pwd(pwd)
+  , m_pwd(pwd ? pwd : "")
 /*
   TODO: Consider refactoring cdk::Session so that constructor
   accepts const reference to ds object. Then instead of m_ds,
@@ -35,6 +63,8 @@ public:
 
   virtual ~Session()
   {}
+
+  friend class Result;
 };
 
 
@@ -42,26 +72,22 @@ class NodeSession: public Session
 {
 public:
 
+  NodeSession(const char* host, unsigned short port,
+              const string  &user,
+              const char    *pwd =NULL)
+   : Session(host, port, user, pwd)
+  {}
+
   NodeSession(unsigned short port,
               const string  &user,
               const char    *pwd =NULL)
    : Session(port, user, pwd)
   {}
 
-  const NodeSession& executeSql(const string&);
-};
-
-
-class Row;
-
-class SqlResult
-{
-public:
-
-  SqlResult(const NodeSession&);
-
-  col_count_t getColumnCount() const;
-  Row* next();
+  Result_init executeSql(const string &query)
+  {
+    return m_sess.sql(query);
+  }
 };
 
 
@@ -69,9 +95,39 @@ class Row
 {
 public:
 
-  const string& getString(unsigned pos);
-  const string& operator[](unsigned pos)
+  virtual ~Row() {}
+
+  virtual const string getString(col_count_t pos) =0;
+  const string operator[](col_count_t pos)
   { return getString(pos); }
+};
+
+
+class Result : nocopy
+{
+  cdk::Reply   m_reply;
+  cdk::Cursor *m_cursor;
+  col_count_t  m_pos;
+
+  class Impl;
+  Impl  *m_impl;
+
+public:
+
+  // TODO: use const references as initializers
+
+  Result(Session::Result_init init);
+  ~Result();
+
+  col_count_t getColumnCount() const
+  {
+    if (!m_cursor)
+      throw "No result set";
+    return m_cursor->col_count();
+  }
+
+  Row* next();
+
 };
 
 
