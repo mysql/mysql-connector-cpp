cfdb0df47246182b0bafc4b7fe12cf2f73e3219f
Luís Silva <luis.m.silva@oracle.com>
2016-11-30 19:26:41 +0000

    Fix for URI with IPv6 address. Add UT with IPv6

--- devapi/tests/session-t.cc
+++ devapi/tests/session-t.cc
@@ -525,3 +525,57 @@ TEST_F(Sess, ssl_session)
   }
 
 }
+
+TEST_F(Sess, ipv6)
+{
+
+  SKIP_IF_NO_XPLUGIN;
+
+  {
+    mysqlx::XSession sess(SessionSettings::HOST, "::1",
+                          SessionSettings::PORT, get_port(),
+                          SessionSettings::USER, get_user(),
+                          SessionSettings::PWD, get_password() ? get_password() : nullptr ,
+                          SessionSettings::SSL_ENABLE, false);
+  }
+
+  //Using URI
+
+  std::stringstream uri;
+
+  uri << "mysqlx://" << get_user();
+
+  if (get_password() && *get_password())
+    uri << ":"<< get_password();
+
+  uri << "@" << "[::1]:" << get_port();
+
+  //URI without ssl_enable
+  {
+    mysqlx::XSession sess(uri.str());
+
+    SqlResult res =  sess.bindToDefaultShard().sql("SHOW STATUS LIKE 'mysqlx_ssl_cipher'").execute();
+
+    auto row = res.fetchOne();
+    cout << row[0] << ":" << row[1] << endl;
+
+    string cipher = row[1];
+
+    EXPECT_TRUE(cipher.empty());
+  }
+
+  //Enable SSL
+  uri << "/?ssl-enable";
+  {
+    mysqlx::XSession sess(uri.str());
+
+    SqlResult res =  sess.bindToDefaultShard().sql("SHOW STATUS LIKE 'mysqlx_ssl_cipher'").execute();
+
+    auto row = res.fetchOne();
+    cout << row[0] << ":" << row[1] << endl;
+
+    string cipher = row[1];
+
+    EXPECT_FALSE(cipher.empty());
+  }
+}
