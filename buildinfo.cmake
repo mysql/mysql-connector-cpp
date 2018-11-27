e01129ea705e9c789f312c3b408591672a81d07d
Rafal Somla <rafal.somla@oracle.com>
2018-11-27 11:14:25 +0100

    cmake: Fixes for INFO_SRC and INFO_BIN generation logic.
    1. Fix error when ENV{PRODUCT_ID} is empty.
    2. Generate INFO_BIN in the build location.
    3. Add INFO_SRC to .gitignore

--- buildinfo.cmake
+++ buildinfo.cmake
@@ -1,36 +1,36 @@
-# Copyright (c) 2018, Oracle and/or its affiliates. All rights reserved. 
-# 
-# This program is free software; you can redistribute it and/or modify 
-# it under the terms of the GNU General Public License, version 2.0, as 
-# published by the Free Software Foundation. 
+# Copyright (c) 2018, Oracle and/or its affiliates. All rights reserved.
 #
-# This program is also distributed with certain software (including 
-# but not limited to OpenSSL) that is licensed under separate terms, 
-# as designated in a particular file or component or in included license 
-# documentation. The authors of MySQL hereby grant you an 
-# additional permission to link the program and your derivative works 
-# with the separately licensed software that they have included with 
-# MySQL. 
-# 
-# Without limiting anything contained in the foregoing, this file, 
-# which is part of MySQL Connector/ODBC, is also subject to the 
-# Universal FOSS Exception, version 1.0, a copy of which can be found at 
-# http://oss.oracle.com/licenses/universal-foss-exception. 
-# 
-# This program is distributed in the hope that it will be useful, but 
-# WITHOUT ANY WARRANTY; without even the implied warranty of 
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
-# See the GNU General Public License, version 2.0, for more details. 
-# 
-# You should have received a copy of the GNU General Public License 
-# along with this program; if not, write to the Free Software Foundation, Inc., 
-# 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 2.0, as
+# published by the Free Software Foundation.
+#
+# This program is also distributed with certain software (including
+# but not limited to OpenSSL) that is licensed under separate terms,
+# as designated in a particular file or component or in included license
+# documentation. The authors of MySQL hereby grant you an
+# additional permission to link the program and your derivative works
+# with the separately licensed software that they have included with
+# MySQL.
+#
+# Without limiting anything contained in the foregoing, this file,
+# which is part of MySQL Connector/ODBC, is also subject to the
+# Universal FOSS Exception, version 1.0, a copy of which can be found at
+# http://oss.oracle.com/licenses/universal-foss-exception.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License, version 2.0, for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 
 ##########################################################################
 
 FUNCTION(GENERATE_INFO_BIN)
 
-  IF(DEFINED ENV{PRODUCT_ID} AND $ENV{PRODUCT_ID} MATCHES "source-dist")
+  IF(DEFINED ENV{PRODUCT_ID} AND "$ENV{PRODUCT_ID}" MATCHES "source-dist")
     MESSAGE("Generating INFO_BIN is skipped for the source package")
     RETURN()
   ENDIF()
@@ -86,8 +86,14 @@ FUNCTION(GENERATE_INFO_BIN)
     SET(INFO_MACOS_BUILD "mysql-version        :  ${MYSQL_VERSION}\n")
   ENDIF()
 
-  CONFIGURE_FILE(INFO_BIN.in "${CMAKE_SOURCE_DIR}/INFO_BIN")
-  install(FILES "${CMAKE_SOURCE_DIR}/INFO_BIN" DESTINATION . COMPONENT Readme)
+  CONFIGURE_FILE(INFO_BIN.in "${CMAKE_BINARY_DIR}/INFO_BIN")
+
+  install(
+    FILES "${CMAKE_BINARY_DIR}/INFO_BIN"
+    DESTINATION ${INSTALL_DOC_DIR}
+    COMPONENT Readme
+  )
+
 ENDFUNCTION()
 
 include(version.cmake)
@@ -167,7 +173,13 @@ FUNCTION(GENERATE_INFO_SRC)
 
     CONFIGURE_FILE(INFO_SRC.in "${CMAKE_SOURCE_DIR}/INFO_SRC")
   ENDIF()
-  install(FILES "${CMAKE_SOURCE_DIR}/INFO_SRC" DESTINATION . COMPONENT Readme)
+
+  install(
+    FILES "${CMAKE_SOURCE_DIR}/INFO_SRC"
+    DESTINATION ${INSTALL_DOC_DIR}
+    COMPONENT Readme
+  )
+
 ENDFUNCTION()
 
 GENERATE_INFO_SRC()
