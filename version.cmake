3747193997424eb1ec58bd8922c19ca49bd6ce6e
Rafał Somla <rafal.somla@oracle.com>
2025-09-19 08:51:54 +0200

    Bump connector version to 9.5.0

--- version.cmake
+++ version.cmake
@@ -35,7 +35,7 @@ set(COPYRIGHT_YEAR "2025" CACHE INTERNAL "version info")
 #
 
 set(CONCPP_VERSION_MAJOR  9 CACHE INTERNAL "version info")
-set(CONCPP_VERSION_MINOR  4 CACHE INTERNAL "version info")
+set(CONCPP_VERSION_MINOR  5 CACHE INTERNAL "version info")
 set(CONCPP_VERSION_MICRO  0 CACHE INTERNAL "version info")
 # Level is "-alpha", "-beta", empty if GA
 set(CONCPP_VERSION_LEVEL  "" CACHE INTERNAL "version info")
